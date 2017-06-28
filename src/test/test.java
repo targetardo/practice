4054232bef1228e2e34989367c351d009b879f28
rub4inskiy <pashagabela@gmail.com>
2017-06-28 18:07:37 +0300

    Add files via upload

---
+++ src/test/test.java
@@ -0,0 +1,23 @@
+package test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import tpp4.Client;
+
+public class test {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("tpp4");
+		EntityManager em = emf.createEntityManager();
+		String sql = "alter table ZAKAZ add foreign key (IDPRODUCT3)";
+		Query q = em.createNativeQuery(sql);
+		em.getTransaction().begin();
+		q.executeUpdate();
+		em.getTransaction().commit();
+		System.out.println("Client list after update");
+	}
+
+}
